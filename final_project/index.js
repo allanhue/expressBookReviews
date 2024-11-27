f69aeb537cf7427420b9ab480b9a36b8254b9724
allanhue <allanwamngi32@gmail.com>
2024-11-27 01:01:27 -0500

    changes on

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
+const { authenticated, users } = require('./router/auth_users.js');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -11,8 +12,24 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+    if(authenticatedUser(username,password)){
+
+          let accesstoken =jwt.sign({
+              data:password
+          }, 'access' {60*60});
+        let(req.session.authorization)={accesstoken,username}
+        
+        return  res.status(200).send('success login')
+         
+        }else {
+           return  res.status(208).json({message:'invalid logins'})
+        }
+    
+    
+    
+
+    }
+);
  
 const PORT =5000;
 
