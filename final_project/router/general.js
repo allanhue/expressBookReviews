d57304d539fe347f30a8179941fe3a7df551c6ef
allanhue <allanwamngi32@gmail.com>
2024-11-27 02:30:25 -0500

    make me

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,25 +12,28 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
+    const public_users=req.body.books
+    if(!public_users==books){
+    }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const ISBN=req.params.isbn
+  res.send({message:'all books retrived under isbn'})
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   const author=req.params.author
+   res.send([books],author)
+  return res.status(300).json({message: " all book"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
+    
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
